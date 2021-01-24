828c98d3e4e2da53f23cfd0f6496e3d11ebaf4aa
Map66 <72113548+Map66@users.noreply.github.com>
2021-01-24 17:48:19 -0500

    Add files via upload

---
+++ Module1/M01 Chapter 01 Chapter Tutorial/plants.js
@@ -0,0 +1,16 @@
+	/*
+      JavaScript 6th Edition
+      Chapter 1
+      Chapter case
+
+      Tinley Xeriscapes
+      Plants web page
+      Author: Michael Peck
+      Date:   1/22/2021
+
+      Filename: plants.htm
+   */
+   // define variables containing img src values
+		var blanket = "images/blanket.jpg";
+		var bluestem = "images/bluestem.jpg";
+		var rugosa = "images/rugosa.jpg";
